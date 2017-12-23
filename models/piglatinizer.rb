b77f6f30a8bb69f92797c2937f42f50ad8873eb6
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2017-12-23 09:53:19 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,10 +1,10 @@
 class PigLatinizer
   def piglatinize(word)
     n = 0
+    binding.pry
     while word.length > n || !is_vowel(word[n])
       n+=1
     end
-    binding.pry
     starting_string = word[n..word.length-1]
     if n > 1
       ending_string = word[0..n]+"ay"
