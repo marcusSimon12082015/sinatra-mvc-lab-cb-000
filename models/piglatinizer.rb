8bcdc7661253627e150725ed9792de1ee43b59b9
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2017-12-23 10:17:51 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -3,9 +3,9 @@ class PigLatinizer
     n = 0
     while word.length > n && is_vowel?(word[n]) == false do
       n+=1
-      binding.pry
     end
     starting_string = word[n..word.length-1]
+    binding.pry
     if n > 1
       ending_string = word[0..n]+"ay"
     else
