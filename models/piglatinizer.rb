3548d0ef8a90cfe0f615687cbeaec2f4d4ecd2c4
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2017-12-23 09:58:14 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -4,6 +4,7 @@ class PigLatinizer
     while word.length > n || is_vowel(word[n])
       n+=1
     end
+    binding.pry
     starting_string = word[n..word.length-1]
     if n > 1
       ending_string = word[0..n]+"ay"
@@ -11,7 +12,6 @@ class PigLatinizer
       ending_string = word[0]+"ay"
     end
     starting_string + ending_string
-    binding.pry
   end
   private
   def is_vowel(letter)
