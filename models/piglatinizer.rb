f4c893897ff04d14247b80975647955646988498
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2017-12-23 09:48:42 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,10 +1,10 @@
 class PigLatinizer
   def piglatinize(word)
+    binding.pry
     n = 0
     while word.length > n || !is_vowel(word[n])
       n+=1
     end
-    binding.pry
     starting_string = word[n..word.size-1]
     ending_string = word[0..n]+"ay"
   end
